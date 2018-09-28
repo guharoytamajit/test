06f61e1baa2a8575ba12c8cb821ee2bd723bf48b
tamajit <guharoytamajit@gmail.com>
2018-09-28 16:09:37 +0530

    init commit

---
+++ src/test/Main.java
@@ -0,0 +1,9 @@
+package test;
+
+import test.util.Calculator;
+
+public class Main {
+public static void main(String[] args) {
+	System.out.println(Calculator.add(3, 8));
+}
+}
---
+++ src/test/util/Calculator.java
@@ -0,0 +1,8 @@
+package test.util;
+
+public class Calculator {
+public static int  add(int a,int b) {
+	//adding and returning
+	return a+b;
+}
+}
