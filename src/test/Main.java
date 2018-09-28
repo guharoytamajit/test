e0aea5f01fe4873d93ab0d807e4cee4c9424af6d
tamajit <guharoytamajit@gmail.com>
2018-09-28 16:22:56 +0530

    another commit

--- src/test/Main.java
+++ src/test/Main.java
@@ -4,6 +4,6 @@ import test.util.Calculator;
 
 public class Main {
 public static void main(String[] args) {
-	System.out.println(Calculator.add(3, 8));
+	System.out.println(Calculator.add(3, 9));
 }
 }
